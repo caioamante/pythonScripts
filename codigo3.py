babe4d1e6ce0bb48387e1187d792fe7651ffb285
caioamante <67496610+caioamante@users.noreply.github.com>
2021-03-03 05:34:28 -0300

    Create codigo3.py

---
+++ codigo3.py
@@ -0,0 +1,6 @@
+from pyspark.sql.types import * 
+schema = StructType ([ StructField ( "author" , StringType (), False ), 
+StructField ( "title" , StringType (), False ),
+ StructField ( "pages" , IntegerType (), False )]) 
+
+ 
